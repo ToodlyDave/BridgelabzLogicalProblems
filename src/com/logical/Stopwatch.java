408fb3c8f22a9531e74b135cd74bb5b3c24b2318
ToodlyDave <alapatdavid@gmail.com>
2021-11-08 11:02:53 +0530

    Refactor the stopwatch program by adding while loop

--- src/com/logical/Stopwatch.java
+++ src/com/logical/Stopwatch.java
@@ -6,16 +6,21 @@ public class Stopwatch {
 
 	public void stopWatch() {
 		Scanner scan = new Scanner(System.in);
+		int n = 0;
 		
 		System.out.print(" Enter some input to start: ");
-		scan.next();
-		
+		scan.next();		
 		long start = System.nanoTime();
-		
-		System.out.print(" Enter some input to end: ");
-		scan.next();
-		
-		long end = System.nanoTime();
+		long end = 0;
+		while (n != 1) {
+			System.out.print(" Please enter 1 to stop: ");
+			n = scan.nextInt();			
+			switch (n) {
+				case 1: 
+					end = System.nanoTime();
+					break;
+			}
+		}
 		
 		System.out.println(" The total time is: " + ((end - start) * Math.pow(10, -9)) );
 		scan.close();
